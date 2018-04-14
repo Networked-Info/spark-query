896c9be6862847d2659497d13e737ce1c3939aa4
WhiteShorthair <chensi@brandeis.edu>
2018-04-13 22:18:37 -0400

    add getSnippest method

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -1,9 +1,10 @@
-import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.spark.SparkConf;
@@ -11,6 +12,9 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -22,9 +26,10 @@ public class SparkMain {
 			setAppName("Example Spark App").
 			setMaster("local[*]"); // Delete this line when submitting to cluster
 	
-	public static void main(String[] args) throws IOException {
+	static JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+	
+	public static void main(String[] args) throws IOException, JSONException {
 		
-		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
 		JavaRDD<String> stringJavaRDD = sparkContext.textFile("index");
 		stringJavaRDD.map(new Function<String, JsonObject>() {
 			private static final long serialVersionUID = 1L;
@@ -42,6 +47,7 @@ public class SparkMain {
 			}
 		});
 		
+		// test three methods
 		String word1 = "aap";
 		String word2 = "aard";
 		JavaRDD<JSONObject> jsonRDD = stringJavaRDD.map(new Function<String, JSONObject>() {
@@ -61,6 +67,8 @@ public class SparkMain {
 		rdd1.intersection(rdd2).sortBy(f -> f, true, 1).saveAsTextFile("output/interoutput");
 		rdd1.subtract(rdd2).sortBy(f -> f, true, 1).saveAsTextFile("output/suboutput");
 		
+		JavaRDD<Integer> unionResult = rdd1.union(rdd2).distinct().sortBy(f -> f, true, 1);
+		getSnippest(unionResult);
 		
 		sparkContext.close();
 	}
@@ -77,24 +85,51 @@ public class SparkMain {
 		return set1.subtract(set2).sortBy(f -> f, true, 1);
 	}
 	
-	private static String getSnippest(JavaRDD<Integer> result) throws IOException {
-		Map<Integer, Integer> map = new HashMap<>();
+	// parses out the file id list
+	private static List<Integer> parse(String target, JSONObject json) throws JSONException {
+		List<Integer> files = new ArrayList<Integer>();
+		JSONObject temp = new JSONObject(json.toString());
+		JSONArray arr = temp.getJSONArray(target);
+		
+		for (int i = 0; i < arr.length(); i++) {
+			@SuppressWarnings("unchecked")
+			Iterator<String> iter = arr.getJSONObject(i).keys();
+			files.add(Integer.parseInt(iter.next()));
+		}
+		return files;
+	}
+	
+	private static List<String> getSnippest(JavaRDD<Integer> result) throws IOException {
+		List<String> snippest = new ArrayList<>();
+		
+		// generate the document and csv map
+		NavigableMap<Integer, Integer> map = new TreeMap<Integer,Integer>();
 		BufferedReader br = new BufferedReader(new FileReader("docMap.csv"));
 		String line = "";
 		while ((line = br.readLine()) != null) {
 			String[] info = line.split(",");
 			int csvId = Integer.valueOf(info[0]);
-			int docId = Integer.valueOf(info[1]);
-			
-			map.put(docId, csvId);
-			System.out.println(docId);
+			int startDocId = Integer.valueOf(info[1]);
+			map.put(startDocId, csvId);
 		}
 		
-		JavaSparkContext docContext = new JavaSparkContext(sparkConf);
+		// for each document find corresponding csv file and output the document
 		for (Integer docId : result.collect()) {
+			int csvId = map.floorEntry(docId).getValue();
+			String filename = "data/wiki_0" + csvId + ".csv"; // need modify when run on cluster
 			
+			sparkContext
+				.textFile(filename)
+				.filter(e -> e.split(",")[0].equals(Integer.toString(docId)))
+				.foreach(new VoidFunction<String>(){
+					private static final long serialVersionUID = 1L;
+					public void call(String content) {
+						snippest.add(content);
+						System.out.println(content);
+					}
+				});
 		}
-		return "";
 		
+		return snippest;
 	}
 }
