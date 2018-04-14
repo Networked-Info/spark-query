f86d5a901a1263e3e512fd65182dfd809bfe6722
WhiteShorthair <chensi@brandeis.edu>
2018-04-13 21:12:27 -0400

    add query operation methods

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -32,4 +32,16 @@ public class SparkMain {
 		});
 		sparkContext.close();
 	}
+	
+	private static JavaRDD<Integer> operateAND(JavaRDD<Integer> set1, JavaRDD<Integer> set2) {
+		return set1.intersection(set2).distinct().sortBy(f -> f, true, 1);
+	}
+	
+	private static JavaRDD<Integer> operateOR(JavaRDD<Integer> set1, JavaRDD<Integer> set2) {
+		return set1.union(set2).sortBy(f -> f, true, 1);
+	}
+	
+	private static JavaRDD<Integer> operateSUB(JavaRDD<Integer> set1, JavaRDD<Integer> set2) {
+		return set1.subtract(set2).sortBy(f -> f, true, 1);
+	}
 }
